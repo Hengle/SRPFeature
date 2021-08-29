09eeb922168dfafc7b81807c187e8381f610bf31
tanweijian <tanweijian199412@gmail.com>
2021-08-29 13:55:38 +0800

    平面反射

---
+++ Assets/Scripts/Runtime/Rendering/Pass/PlanarReflectionsPass.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace Game.Runtime
+{
+    [Serializable]
+    public class PlanarReflectionSettings
+    {
+        [Range(0.1f, 2f)]
+        public float RenderScale = 1;
+        public float ClipPlaneOffset = -0.1f;
+        public LayerMask ReflectLayers = -1;
+        public float FarClipPlane = 512;
+    }
+    
+    public class PlanarReflectionsPass : ScriptableRenderPass
+    {
+        private Camera _reflectionsCamera;
+        private PlanarReflectionSettings _settings;
+
+        public PlanarReflectionsPass(PlanarReflectionSettings settings)
+        {
+            _settings = settings;
+        }
+
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+        }
+
+        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+        {
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+        }
+        
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+        }
+    }
+}
